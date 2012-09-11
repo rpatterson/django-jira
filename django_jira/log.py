665288f346ea933824a02f5e63865e293c9e351e
Stephen Golub <nickburns2006@tamu.edu>
2012-09-11 14:48:53 -0500

    Adding log python module to git.

---
+++ django_jira/log.py
@@ -0,0 +1,171 @@
+import logging
+import sys
+import traceback
+
+import json
+
+from django.utils.log import AdminEmailHandler
+from django.views.debug import ExceptionReporter, get_exception_reporter_filter
+
+from jira.client import JIRA
+from jira.exceptions import JIRAError
+
+class JiraRecord(object):
+    def __init__(self, record, exc_info):
+        self.request = record.request
+        self.exc_info = exc_info
+        self.levelname = "ERROR"
+
+    def getMessage(self):
+        return "Problem Working with Jira Server"
+
+class JiraHandler(logging.Handler):
+    """An exception log handler that sends the log entries to the specified
+    Jira server and project.
+
+    If the request is passed as the first arguemtn to the log record, request
+    data will be provided in the Jira Ticket.
+
+    """
+
+    def __init__(self, include_html=False, server_url="http://localhost:2990/jira/",
+            user=False, password=False, auth_type=None, issue_defaults=False,
+            reopen_closed=(4,6), reopen_action=3, wont_fix=False,
+            comment_reopen_only=False, mail_logger="mail_admins"):
+        logging.Handler.__init__(self)
+        self.include_html = include_html
+        if not server_url or not user or not password:
+            self.unused = True
+        else:
+            self.unused = False
+            self.jira_url = server_url
+            self.jira_user = user
+            self.jira_pwd = password
+            self.auth_type = auth_type
+
+            if issue_defaults:
+                self.issue_defaults = issue_defaults.copy()
+            else:
+                self.issue_defaults = {}
+
+            if 'project' not in self.issue_defaults:
+                self.issue_defaults['project'] = {'key': 'JIRA'}
+            elif not isinstance(self.issue_defaults['project'], dict):
+                self.issue_defaults['project'] = {'key':
+                        self.issue_defaults['project']}
+
+            if 'issuetype' not in self.issue_defaults:
+                self.issue_defaults['issuetype'] = {'id': '1'}
+            elif not isinstance(self.issue_defaults['issuetype'], dict):
+                self.issue_defaults['issuetype'] = {'id':
+                        self.issue_defaults['issuetype']}
+
+            self.reopen_closed = reopen_closed
+            self.reopen_action = reopen_action
+            self.wont_fix = wont_fix
+            self.comment_reopen_only = comment_reopen_only
+            self.mail_logger = mail_logger
+
+    @property
+    def _jira(self):
+        if hasattr(self, "_jr") and isinstance(self._jr, JIRA):
+            return self._jr
+        else:
+            if hasattr(self, "jira_url") and hasattr(self,"jira_user") and \
+                    hasattr(self, "jira_pwd"):
+                auth_type = getattr(self, "auth_type", "basic").lower()
+
+                if auth_type == None:
+                    self._jr = JIRA(options={"server": self.jira_url})
+                    return self._jr
+                elif auth_type == "basic":
+                    self._jr = JIRA(basic_auth=(self.jira_user, self.jira_pwd),
+                            options={"server": self.jira_url})
+                    return self._jr
+                elif auth_type == "oauth":
+                    raise Exception("OAuth for the Jira Reporter hasn't been implemented yet.")
+                else:
+                    raise Exception("You have entered an invalid AuthType")
+            else:
+                raise Exception("There is a problem with the configuration of the django-jira app.")
+
+    def fire_email(self, record, exc_info=None):
+        mail_handler = AdminEmailHandler(include_html=self.include_html)
+        mail_handler.emit(record)
+        mail_handler.emit(JiraRecord(record, exc_info))
+
+
+    def emit(self, record):
+        if self.unused:
+            self.fire_email(record)
+
+        # We're first going to construct the strings
+        try:
+            request = record.request
+            subject = record.getMessage()
+            filter = get_exception_reporter_filter(request)
+            request_repr = filter.get_request_repr(request)
+        except Exception:
+            subject = record.getMessage()
+            request = None
+            request_repr = "Request repr() unavailable."
+        subject = self.format_subject(subject)
+
+
+        if record.exc_info:
+            exc_info = record.exc_info
+            stack_trace = '\n'.join(traceback.format_exception(*record.exc_info))
+        else:
+            exc_info = (None, record.getMessage(), None)
+            stack_trace = 'No stack trace available'
+
+        message = "%s\n\n%s" % (stack_trace, request_repr)
+        issue_msg = "%s\n\n{code:title=Traceback}\n%s\n{code}\n\n{code:title=Request}\n%s\n{code}" % (subject, stack_trace, request_repr)
+
+        # See if this exception has already been reported inside JIRA
+        try:
+            existing = self._jira.search_issues('project = "' +
+                    self.issue_defaults['project']["key"] + 
+                    '" AND summary ~ "' + subject + '"', maxResults=1)
+
+            # If it has, add a comment noting that we've had another report of it
+            found = False
+            for issue in existing:
+                if subject == issue.fields.summary:
+
+                    # If this issue is closed, reopen it
+                    if int(issue.fields.status.id) in self.reopen_closed \
+                            and (issue.fields.resolution and int(issue.fields.resolution.id) != self.wont_fix):
+                        self._jira.transition_issue(issue,
+                                str(self.reopen_action))
+
+                        reopened = True
+                    else:
+                        reopened = False
+
+                    # Add a comment
+                    if reopened or not self.comment_reopen_only:
+                        self._jira.add_comment(issue, issue_msg)
+
+                    found = True
+                    break
+
+            if not found:
+                # Otherwise, create it
+                issue = self.issue_defaults.copy()
+                issue['summary'] = subject
+                issue['description'] = issue_msg
+
+                self._jira.create_issue(fields=issue)
+        except Exception:
+            return self.fire_email(record, sys.exc_info())
+
+
+    def format_subject(self, subject):
+        """
+        Escape CR and LF characters, and limit length.
+        RFC 2822's hard limit is 998 characters per line. So, minus "Subject: "
+        the actual subject must be no longer than 989 characters.
+        """
+        formatted_subject = subject.replace('\n', '\\n').replace('\r', '\\r')
+        return formatted_subject[:989]
