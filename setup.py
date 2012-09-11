b687d37b153f8d7df512c34dd974cecbadadb4cc
Stephen Golub <nickburns2006@tamu.edu>
2012-09-11 15:14:09 -0500

    Fixed the requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(name='django-jira',
-      version='2.1.1',
+      version='2.1.2',
       description='An automated exception reporter to JIRA from Django',
       author='Stephen Golub',
       author_email='nickburns2006@tamu.edu',
       url='https://github.com/nickburns2006/django-jira',
       packages=['django_jira'],
-      install_requires=['jira-python',],
+      install_requires=['jira-python', 'django>=1.4'],
      )
